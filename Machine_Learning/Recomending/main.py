3522891d34feec0c422e129ad7d7e635c1972802
dkoshman <dkoshman@nes.ru>
2022-08-31 22:26:58 +0300

    fixed memory leak caused by gradient hooks

--- Machine_Learning/Recomending/main.py
+++ Machine_Learning/Recomending/main.py
@@ -11,77 +11,111 @@ from my_ml_tools.wandb_wrap import WandbCLIWrapper
 
 from data import SparseDataModule
 from lit_module import LitProbabilityMatrixFactorization
-
-
-def build_trainer(config):
-    callbacks = [
-        GradientAccumulationScheduler(
-            scheduling={0: config["n_batches_for_gradient_accumulation"]}
-        )
-    ]
-
-    early_stopping_config = config["early_stopping"]
-    callbacks.append(
-        EarlyStopping(
-            monitor=early_stopping_config["monitor"],
-            mode=early_stopping_config["mode"],
-            min_delta=early_stopping_config["min_delta"],
-            patience=early_stopping_config["patience"],
-        )
-    )
-    if (swa_config := config["stochastic_weight_averaging"])["enable"]:
-        callbacks.append(
-            StochasticWeightAveraging(
-                swa_epoch_start=swa_config["swa_epoch_start"],
-                swa_lrs=swa_config["swa_lrs"],
-                annealing_epochs=swa_config["annealing_epochs"],
-                annealing_strategy=swa_config["annealing_strategy"],
-            )
-        )
-    amp_backend = config["automatic_mixed_precision"]["amp_backend"]
-    amp_level = (
-        config["automatic_mixed_precision"]["amp_level"]
-        if amp_backend == "apex"
-        else None
-    )
-    trainer = pl.Trainer(
-        logger=WandbLogger(),
-        check_val_every_n_epoch=config["check_val_every_n_epoch"],
-        default_root_dir="local",
-        max_epochs=config["max_epochs"],
-        accelerator="gpu",
-        gpus=[config["gpu"]],
-        callbacks=callbacks,
-        amp_backend=amp_backend,
-        amp_level=amp_level,
-    )
-    return trainer
+from model import (
+    ConstrainedProbabilityMatrixFactorization,
+    ProbabilityMatrixFactorization,
+)
 
 
 class RecommendingWandbCLIWrapper(WandbCLIWrapper):
+    def build_datamodule(self, config):
+        self.datamodule = SparseDataModule(
+            batch_size=config["batch_size"], num_workers=config["num_workers"]
+        )
+        self.n_users = self.datamodule.train_dataset.shape[0]
+        self.n_items = self.datamodule.train_dataset.shape[1]
+        return self.datamodule
+
+    def build_model(self, config):
+        if config["model"] == "probabilistic_matrix_factorization":
+            self.model = ProbabilityMatrixFactorization(
+                n_users=self.n_users,
+                n_items=self.n_items,
+                latent_dimension=config["latent_dimension"],
+                regularization_lambda=config["weight_decay"],
+            )
+        elif config["model"] == "constrained_probabilistic_matrix_factorization":
+            self.model = ConstrainedProbabilityMatrixFactorization(
+                n_users=self.n_users,
+                n_items=self.n_items,
+                latent_dimension=config["latent_dimension"],
+                regularization_lambda=config["weight_decay"],
+                implicit_feedback=self.datamodule.train_dataset.implicit_feedback,
+            )
+        else:
+            raise ValueError(f"Unknown model type {config['model']}")
+
+        return self.model
+
+    def build_lightning_module(self, model, config):
+        self.lit_module = LitProbabilityMatrixFactorization(
+            model=model,
+            n_items=self.n_items,
+            optimizer_kwargs=config["optimizer_kwargs"],
+        )
+        return self.lit_module
+
+    def build_callbacks(self, config):
+        early_stopping_config = config["early_stopping"]
+        swa_config = config["stochastic_weight_averaging"]
+
+        callbacks = [
+            GradientAccumulationScheduler(
+                scheduling={0: config["n_batches_for_gradient_accumulation"]}
+            ),
+            EarlyStopping(
+                monitor=early_stopping_config["monitor"],
+                mode=early_stopping_config["mode"],
+                min_delta=early_stopping_config["min_delta"],
+                patience=early_stopping_config["patience"],
+            ),
+        ]
+
+        if swa_config["enable"]:
+            callbacks.append(
+                StochasticWeightAveraging(
+                    swa_epoch_start=swa_config["swa_epoch_start"],
+                    swa_lrs=swa_config["swa_lrs"],
+                    annealing_epochs=swa_config["annealing_epochs"],
+                    annealing_strategy=swa_config["annealing_strategy"],
+                )
+            )
+
+        return callbacks
+
+    def build_trainer(self, config):
+        callbacks = self.build_callbacks(config)
+
+        amp_config = config["automatic_mixed_precision"]
+        amp_backend = amp_config["amp_backend"]
+        amp_level = (
+            amp_config["amp_level"] if amp_config["amp_backend"] == "apex" else None
+        )
+
+        self.trainer = pl.Trainer(
+            logger=WandbLogger(),
+            check_val_every_n_epoch=config["check_val_every_n_epoch"],
+            default_root_dir="local",
+            max_epochs=config["max_epochs"],
+            accelerator="gpu",
+            gpus=[config["gpu"]],
+            callbacks=callbacks,
+            amp_backend=amp_backend,
+            amp_level=amp_level,
+        )
+        return self.trainer
+
     def debug_config(self, config):
         config["check_val_every_n_epoch"] = 1
         return config
 
-
-@RecommendingWandbCLIWrapper
-def main(config):
-    datamodule = SparseDataModule(
-        batch_size=config["batch_size"], num_workers=config["num_workers"]
-    )
-
-    lit_module = LitProbabilityMatrixFactorization(
-        n_users=datamodule.train_dataset.shape[0],
-        n_items=datamodule.train_dataset.shape[1],
-        latent_dimension=config["latent_dimension"],
-        weight_decay=config["weight_decay"],
-        optimizer_kwargs=config["optimizer_kwargs"],
-    )
-
-    trainer = build_trainer(config)
-
-    trainer.fit(lit_module, datamodule=datamodule)
+    def main(self, config):
+        datamodule = self.build_datamodule(config)
+        model = self.build_model(config)
+        lit_module = self.build_lightning_module(model, config)
+        trainer = self.build_trainer(config)
+        trainer.fit(lit_module, datamodule=datamodule)
 
 
 if __name__ == "__main__":
-    main()
+    RecommendingWandbCLIWrapper().launch_agent()
--- Machine_Learning/Recomending/model.py
+++ Machine_Learning/Recomending/model.py
@@ -1,6 +1,18 @@
 import torch
 
 from my_ml_tools.models import weight_decay
+from my_ml_tools.utils import scipy_to_torch_sparse
+
+
+def _build_weight(*dimensions):
+    weight = torch.nn.Parameter(torch.empty(*dimensions))
+    torch.nn.init.xavier_normal_(weight)
+    return weight
+
+
+def _build_bias(*dimensions):
+    bias = torch.nn.Parameter(torch.zeros(*dimensions))
+    return bias
 
 
 class ProbabilityMatrixFactorization(torch.nn.Module):
@@ -11,13 +23,11 @@ class ProbabilityMatrixFactorization(torch.nn.Module):
 
         self.regularization_lambda = regularization_lambda
 
-        self.user_weight = torch.nn.Parameter(torch.empty(n_users, latent_dimension))
-        torch.nn.init.xavier_normal_(self.user_weight)
-        self.user_bias = torch.nn.Parameter(torch.zeros(n_users, 1))
+        self.user_weight = _build_weight(n_users, latent_dimension)
+        self.user_bias = _build_bias(n_users, 1)
 
-        self.item_weight = torch.nn.Parameter(torch.empty(n_items, latent_dimension))
-        torch.nn.init.xavier_normal_(self.item_weight)
-        self.item_bias = torch.nn.Parameter(torch.zeros(n_items, 1))
+        self.item_weight = _build_weight(n_items, latent_dimension)
+        self.item_bias = _build_bias(n_items, 1)
 
         self.bias = torch.nn.Parameter(torch.zeros(1))
 
@@ -33,9 +43,60 @@ class ProbabilityMatrixFactorization(torch.nn.Module):
         rating = user_weight @ item_weight.T + user_bias + item_bias.T + self.bias
         rating = self.sigmoid(rating)
 
+        # Need to add regularization here because otherwise optimizer will decay all weights,
+        # not only those corresponding to user and item ids.
+        for parameter in [user_weight, item_weight, user_bias, item_bias]:
+            weight_decay(parameter, self.regularization_lambda)
+
+        return rating
+
+
+class ConstrainedProbabilityMatrixFactorization(torch.nn.Module):
+    def __init__(
+        self,
+        n_users,
+        n_items,
+        latent_dimension,
+        regularization_lambda,
+        implicit_feedback,
+    ):
+        super().__init__()
+
+        self.regularization_lambda = regularization_lambda
+
+        self.user_weight = _build_weight(n_users, latent_dimension)
+        self.item_weight = _build_weight(n_items, latent_dimension)
+        self.item_rating_effect_weight = _build_weight(n_items, latent_dimension)
+        self.bias = torch.nn.Parameter(torch.zeros(1))
+
+        self.implicit_feedback = implicit_feedback
+        self.implicit_feedback_norm = torch.tensor(implicit_feedback.sum(axis=1)) + 1e-8
+
+        self.sigmoid = torch.nn.Sigmoid()
+
+    def forward(self, user_ids, item_ids):
+        #ODO: add bias?
+        user_weight = self.user_weight[user_ids]
+        item_weight = self.item_weight[item_ids]
+        # Need to clone to avoid gradient hook accumulation on same tensor and subsequent memory leak
+        item_rating_effect_weight = self.item_rating_effect_weight.clone()
+
+        implicit_feedback = scipy_to_torch_sparse(
+            self.implicit_feedback[user_ids.cpu().numpy()], device=user_weight.device
+        )
+        implicit_feedback_norm = self.implicit_feedback_norm[user_ids].to(
+            user_weight.device
+        )
+
+        user_factors = (
+            user_weight
+            + implicit_feedback @ item_rating_effect_weight / implicit_feedback_norm
+        )
+        rating = user_factors @ item_weight.T + self.bias
+        rating = self.sigmoid(rating)
+
         weight_decay(user_weight, self.regularization_lambda)
         weight_decay(item_weight, self.regularization_lambda)
-        weight_decay(user_bias, self.regularization_lambda)
-        weight_decay(item_bias, self.regularization_lambda)
+        weight_decay(item_rating_effect_weight, self.regularization_lambda)
 
         return rating
